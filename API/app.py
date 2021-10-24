9d4bedc9c70ecd92fe1eb5d88048c5c4ac87eecb
Nikita <nikshvora@gmail.com>
2021-10-24 11:55:36 +0300

    Отправка файлов на сервер

--- API/app.py
+++ API/app.py
@@ -90,6 +90,7 @@ def save_file_from_tg():
     # print(data_files)
     counter = 0
     for data in data_files:
+        print(data[1])
         file_name = secure_filename(data[1].filename)
         os.mkdir(f'/home/pdf/kekostan/API/users_files/{data[0]}')
         data[1].save(f"/home/pdf/kekostan/API/users_files/{data[0]}/{file_name}")
